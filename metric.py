eee32ca78bf32e3b9b4e1e7d5fe815d98e3d1317
IainChapman <56636026+IainChapman@users.noreply.github.com>
2019-10-20 18:23:27 +0100

    Add files via upload

--- metric.py
+++ metric.py
@@ -63,11 +63,12 @@ class Metric():
         return trigsimp(expand_trig(simplify(sum)))
     
     def ricci(self):
-        ricciArray = MutableDenseNDimArray([0] * 4, [2, 2])
+        ricciArray = MutableDenseNDimArray([0] * self.dimension * self.dimension, [self.dimension, self.dimension])
         for i in range(2):
             for j in range(2):
                 sum = 0
                 for m in range(self.dimension):
+                    # note unusual contraction used in RGC 
                     sum += self.riemann(m, i, j, m)
                 ricciArray[i, j] = sum
         return ricciArray
@@ -79,6 +80,7 @@ class Metric():
         for m in range(self.dimension):
             for n in range(self.dimension):
                 sum += dual[m, n] * ricciArray[m, n]
+        # usual CAS kick to properly simplify trig expressions
         return trigsimp(expand_trig(simplify(sum)))
         
     def geodesicEquation(self, i):
@@ -88,11 +90,14 @@ class Metric():
             for j in range(self.dimension):
                 for k in range(self.dimension):
                     myEquation += self.connection (i, j, k) * Derivative (self.coordinates[j], lamda) * Derivative (self.coordinates[k], lamda)
+        # put into '= 0' form for clarity
         return Eq(myEquation,0)
     
     def gaussianCurvature (self):
+        # lower the first index on the magic Riemann component
         r_0101 = 0
         for  i in range(self.dimension):
             r_0101 += self.array[i,0]*self.riemann(i,1,0,1)
+        # divide by the determinant of the metric
         g = self.array.tomatrix().det()
         return r_0101 / g
